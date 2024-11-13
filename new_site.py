5f8726afdcdf25e5706a2ba58d2af515ba4822a1
Karoly Czovek <kc@signatrix.com>
2024-11-13 16:19:25 +0100

    Syntax fix

--- new_site.py
+++ new_site.py
@@ -55,7 +55,7 @@ class CreateSiteWithSubnetsScript(Script):
     )
 
     contact_role = ObjectVar(
-        description="Contact person name",
+        description="Contact persons role",
         model=ContactRole
     )
 
