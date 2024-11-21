f91ef91907fc316954b2e1774d22a8ab40948958
Karoly Czovek <kc@signatrix.com>
2024-11-21 14:18:03 +0100

    Added confluence export

--- device_platforms.py
+++ device_platforms.py
@@ -1,17 +1,100 @@
 from dcim.models import Device, DeviceRole
-from extras.scripts import Script
+from extras.scripts import *
 from django.utils.html import format_html
 from dcim.choices import DeviceStatusChoices
+from requests.auth import HTTPBasicAuth
+from django.conf import settings
+import requests
+from datetime import datetime
+
 
 
 class CartwatchVersionsScript(Script):
     class Meta:
         name = "Cartwatch Versions"
-        description = "Displays all servers with their Cartwatch and Cartwatch Admin versions"
+        description = "Displays all servers with their Cartwatch and Cartwatch_Admin versions"
+
+    update_confluence_page = BooleanVar(
+        description="Update Confluence page too",
+        default=False
+    )
+
+    def update_confluence(self, data, article_body):
+        if data.update_confluence_page:
+            confluence_config = getattr(settings, 'PLUGINS_CONFIG', {}).get(
+                'netbox_confluence_kb', {})
+
+            CONFLUENCE_INSTANCE = confluence_config.get(
+                'confluence_cloud_instance')
+            CONFLUENCE_API_TOKEN = confluence_config.get(
+                'token')
+            CONFLUENCE_USER = confluence_config.get(
+                'confluence_user')
+
+            CONFLUENCE_AUTH = HTTPBasicAuth(CONFLUENCE_USER, CONFLUENCE_API_TOKEN)
+
+            CONFLUENCE_URL = f"https://{CONFLUENCE_INSTANCE}.atlassian.net/wiki/rest/api/content"
+            CONFLUENCE_PAGE_ID = "3261431823"
+
+            if not CONFLUENCE_URL or not CONFLUENCE_API_TOKEN:
+                self.log_error("Confluence URL or token not found in settings")
+                return
+
+            headers = {
+                'Content-Type': 'application/json'
+            }
+
+            confluence_content = f"""
+                <h1>Deployed Cartwatch Versions</h1>
+                <p>Last updated: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}</p>
+                {article_body}
+                """
+            try:
+                # Get current version
+                response = requests.get(
+                        f"{CONFLUENCE_URL}/{CONFLUENCE_PAGE_ID}",
+                        headers=headers,
+                        auth=CONFLUENCE_AUTH
+                )
+                response.raise_for_status()
+                version = response.json()['version']['number']
+
+                # Update page
+                payload = {
+                    'version': {'number': version + 1},
+                    'title': 'Cartwatch Versions',
+                    'type': 'page',
+                    'body': {
+                            'storage': {
+                                'value': confluence_content,
+                                'representation': 'storage'
+                            }
+                        }
+                    }
+
+                response = requests.put(
+                        f"{CONFLUENCE_URL}/{CONFLUENCE_PAGE_ID}",
+                        headers=headers,
+                        json=payload,
+                        auth=CONFLUENCE_AUTH
+                )
+                response.raise_for_status()
+                self.log_success("Successfully updated Confluence page")
+
+            except requests.exceptions.RequestException as e:
+                self.log_error(f"Failed to update Confluence page: {str(e)}")
+        else:
+            self.log_error("Confluence configuration not found in settings")
 
     def run(self, data, commit):
         output = []
         server_role = DeviceRole.objects.get(name='server')
+
+        html = '<table class="table"><thead><tr>'
+        headers = ['Device', 'Platform', 'Cartwatch', 'Cartwatch Admin']
+        for header in headers:
+            html += f'<th>{header}</th>'
+        html += '</tr></thead><tbody>'
 
         for device in Device.objects.filter(
             status=DeviceStatusChoices.STATUS_ACTIVE,
@@ -20,7 +103,16 @@ class CartwatchVersionsScript(Script):
         ):
             # Change the naming standard based on the re.match
             ol = f"{device.name} on {device.platform.name if device.platform else 'N/A'} deployed at {device.site.name} with cartwatch {device.custom_field_data.get('cartwatch_version', 'N/A')} and cartwatch_admin {device.custom_field_data.get('cartwatch_admin_version', 'N/A')}"
+
+            html += f'<tr><td>{device.name}</td>'
+            html += f'<td>{device.platform.name if device.platform else "N/A"}</td>'
+            html += f'<td>{device.custom_field_data.get("cartwatch_version", "N/A")}</td>'
+            html += f'<td>{device.custom_field_data.get("cartwatch_admin_version", "N/A")}</td></tr>'
+
             output.append(ol)
             # self.log_success(ol)
+
+        html += '</tbody></table>'
+        self.update_confluence(article_body=html)
 
         return '\n'.join(output)
