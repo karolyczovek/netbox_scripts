c41e33c80ca6520e3a5f35dadecc006685ea40f2
Karoly Czovek <kc@signatrix.com>
2024-11-21 13:25:39 +0100

    Add site name to device report

--- device_platforms.py
+++ device_platforms.py
@@ -18,7 +18,7 @@ class CartwatchVersionsScript(Script):
             role=server_role
         ):
             # Change the naming standard based on the re.match
-            ol = f"{device.name} on {device.platform.name if device.platform else 'N/A'} deployed cartwatch {device.custom_field_data.get('cartwatch_version', 'N/A')} and cartwatch_admin {device.custom_field_data.get('cartwatch_admin_version', 'N/A')}"
+            ol = f"{device.name} on {device.platform.name if device.platform else 'N/A'} deployed at {device.site.name} with cartwatch {device.custom_field_data.get('cartwatch_version', 'N/A')} and cartwatch_admin {device.custom_field_data.get('cartwatch_admin_version', 'N/A')}"
             output.append(ol)
             # self.log_success(ol)
 
